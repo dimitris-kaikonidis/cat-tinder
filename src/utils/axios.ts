de5060f708b3798911c19821ff4128ab09c22e08
Dimitris Kaikonidis <dimitris.kaikonidis@gmail.com>
2021-07-30 17:42:20 +0200

    heroku pt3

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,10 +1,9 @@
-import { CAT_SECRET } from "./secrets.json";
 import axios from "axios";
 
 const instance = axios.create({
     baseURL: "https://api.thecatapi.com/v1",
     headers: {
-        "x-api-key": process.env.CAT_SECRET || CAT_SECRET,
+        "x-api-key": process.env.CAT_SECRET || require("./secrets.json").CAT_SECRET,
     },
 });
 
